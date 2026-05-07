3c80222345a8d9d3facabb31ceb2c5f9f2096f8b
pokja0 <pokja0materikaper@gmail.com>
2026-05-07 10:10:43 +0800

    7 mei

--- app.R
+++ app.R
@@ -67,7 +67,7 @@ ui <- page_navbar(
         "Tabel",
         # card_title("Tabel Rekap Aktivitas Srikandi Pegawai"),
         h1("Tabel Rekap Aktivitas Srikandi Pegawai", style="text-align: center;"),
-        h6("Sumber Data: srikandi.arsip.go.id (diakses tanggal 27 April 2026)", style="text-align: center;"),
+        h6("Sumber Data: srikandi.arsip.go.id (diakses tanggal 7 Mei 2026)", style="text-align: center;"),
         layout_columns(
           col_widths = 2,
           downloadButton("download_excel_srikandi_pegawai", "Download Excel")
@@ -244,7 +244,7 @@ server <- function(input, output, session) {
       decimal.mark = ","
     )
   })
-  output$tanggal_scraping <- renderText({ "27 April 2026 - Pukul 13.00 WITA"})
+  output$tanggal_scraping <- renderText({ "7 Mei 2026 - Pukul 09.00 WITA"})
   
   output$surat_terbaca <- renderText({ 
     paste0(comma(
@@ -267,7 +267,7 @@ server <- function(input, output, session) {
     reactable(
       srikandi_pegawai |>
         select(-NIP),
-      #groupBy = "Nama",
+      groupBy = "Nama",
       defaultColDef = colDef(
         align = "center"
       ),
